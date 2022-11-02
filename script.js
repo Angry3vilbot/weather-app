d083f22645f20936fe54d43db9915429a1c0f5e0
Angry3vlibot <myh.balaker@gmail.com>
2022-11-03 00:26:35 +0200

    Add HTML tags, write basic styling, function to load text data into DOM

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-let dataObject
+const searchbox = document.getElementById('country')
+const searchBtn = document.getElementById('submit-weather')
+const mainCard = document.getElementById('main-card')
+const secondaryCards = document.querySelectorAll('.temperature-card')
 
-async function getWeatherData(location){
+let dataObject
+let weatherArray = []
+
+async function getWeatherData(){
+    let location = searchbox.value
     let forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&id=524901&appid=b7e3474cceef0623afc10707abc1053a`)
     dataObject = await forecast.json()
+    getWeatherArray()
+    loadWeatherIntoDOM()
 }
 
 function getCityName(){
-    console.log(dataObject.city.name)
+    return dataObject.city.name
 }
 
-function getTemperatureArray(){
+function getWeatherArray(){
     let readings = dataObject.list
-    let temperatureArray = []
-    temperatureArray = readings.slice(0, 11)
-    console.log(temperatureArray)
+    weatherArray = readings.slice(0, 11)
+    console.log(weatherArray)
 }
+
+function getTemperature(){
+    let newTempsArray = []
+    weatherArray.forEach(reading => newTempsArray.push(Math.ceil(reading.main.temp - 273.15)))
+    return newTempsArray
+}
+
+function getWeather(){
+    weatherArray.forEach(reading => console.log(`${reading.weather[0].main} -----> ${reading.weather[0].description}`))
+}
+
+function loadWeatherIntoDOM(){
+    const weather = mainCard.querySelector('div').querySelector('#weather')
+    const temp = mainCard.querySelector('div').querySelector('#temperature-now')
+    const time = mainCard.querySelector('div').querySelector('p')
+
+    let temperatureArray = getTemperature()
+    temp.innerHTML = `The temperature is ${temperatureArray[0]}°C`
+    weather.innerHTML = weatherArray[0].weather[0].main
+    time.innerHTML = weatherArray[0].dt_txt
+
+    for(let i = 1; i < weatherArray.length; i++){
+        secondaryCards[i-1].querySelector('h2').innerHTML = weatherArray[i].dt_txt.slice(11, 16)
+        secondaryCards[i-1].querySelector('p').innerHTML = `${temperatureArray[i]}°C`
+    }
+}
+
+searchBtn.addEventListener('click', getWeatherData)
